4dec57e6f589eda01af084349f79f9a5a551846a
Zack-Violet <56907212+Zack-Violet@users.noreply.github.com>
2022-03-28 10:33:49 +0800

    Create ObjectAdapter.cpp

---
+++ adapter/ObjectAdapter.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory>
+
+class Target
+{
+    public:
+        virtual ~Target() {}
+        virtual void request() = 0;
+};
+
+class Adaptee
+{
+    public:
+        void specificRequest()
+        {
+            std::cout << "do something specificRequest()" << std::endl;
+        }
+};
+
+class Adapter : public Target
+{
+    public:
+        Adapter() : apt() {}
+        ~Adapter()
+        {
+            if (apt)
+                delete apt;
+        }
+        virtual void request()
+        {
+            apt->specificRequest();
+        }
+
+    private:
+        Adaptee* apt;
+};
+
+int main()
+{
+    std::unique_ptr<Target> ptr(new Adapter());
+    ptr->request();
+
+    return 0;
+}
